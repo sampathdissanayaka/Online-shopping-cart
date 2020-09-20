00efd4f912edaf67c80a98ff4a9251b28ad73e3a
unknown <Sampath Dissanayaka>
2020-09-21 02:55:41 +0700

    create login & sign in page

---
+++ signin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>online shopping cart</title>
+<style>
+
+.login ul li {
+    float:left;
+    margin-left:25px;
+    list-style-type:none;
+}
+
+a{
+  text-decoration:none;
+}
+</style>
+</head>
+
+<body>
+<div class = "login">
+<ul>
+<li><a href="login.php">Login</a></li>
+<li><a href="signin.php">Sign in</a></li>
+</ul>
+</div>
+</body>
+
+</html>
